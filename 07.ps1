3e920a6ecd6f6fa0858c6f28f919b32ac9885de7
planetacomputer <evapardeza123@gmail.com>
2022-02-23 10:05:51 +0100

    Update 07.ps1

--- 07.ps1
+++ 07.ps1
@@ -2,7 +2,3 @@
 #Arrodoneix a enter el nombre de dies que han passat des de la seva data de naixement.
 #Desa'l a la variable $dies i retorna-la
 #Hauràs de fer servir un objecte TimeSpan
-[datetime]$born = "18 July 1918"
-$today = Get-Date
-$dies=[math]::Round(($a-$born).TotalDays)
-return $dies
