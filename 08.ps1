161cd323ca300da18678707644a8bf419c166f87
planetacomputer <evapardeza123@gmail.com>
2022-02-23 10:06:10 +0100

    Update 08.ps1

--- 08.ps1
+++ 08.ps1
@@ -1,6 +1,3 @@
 #Pren el string "supercalifragilisticoespialidoso" i retorna un altre amb les lletres ordenades alfabéticament
 #Has de tenir en compte que un string es pot passar a array... i la inversa. Investiga
 $Inputstring ="supercalifragilisticoespialidoso"
-$CharArray =$InputString.ToCharArray()
-$b = $CharArray|sort
-return $b -join ""
