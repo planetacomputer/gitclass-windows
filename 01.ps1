4ff29c8d9bac95b2ef27e79064f29744facb2b7c
planetacomputer <evapardeza123@gmail.com>
2022-02-23 10:03:45 +0100

    Update 01.ps1

--- 01.ps1
+++ 01.ps1
@@ -1,4 +1,1 @@
-#01.ps1 Escriu "Hello World" en PowerShell
-New-LocalUser -Name dani -NoPassword
-Get-LocalUser
-Invoke-WebRequest -Uri http://ipv4.download.thinkbroadband.com/5MB.zip -OutFile File
+#01.ps1 Crea un usuari anomenat dani, sense password
